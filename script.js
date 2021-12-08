13f0c3fea041a6394411bbd517e2f6a5d8db3c64
SahandGN <sahand.gn@gmail.com>
2021-12-08 16:36:54 +0330

    added smthing else

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-$(document).ready(function () {
-    $("button").click(function () {
-        $("p").toggle();
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").slideToggle("slow");
     });
 });
